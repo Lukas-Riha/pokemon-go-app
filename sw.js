b952b4857272c3db4beeb89d0e285178057bbbd3
Lukas <lukasriha.it@gmail.com>
2026-09-09 09:37:27 +0200

    appka 2026-09-09 09:37

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Generováno tools/build_publish.py — needitovat ručně. */
-var VERZE = "pgo-44971fb8f378";
+var VERZE = "pgo-ede78297d705";
 var SOUBORY = ["./", "index.html", "manifest.webmanifest",
   "ikona-192.png", "ikona-512.png", "strop.html"];
 
